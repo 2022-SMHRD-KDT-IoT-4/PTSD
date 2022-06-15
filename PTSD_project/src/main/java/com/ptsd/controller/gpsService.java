f3b53d2433b67c2e9595feb28b90a483cb320b48
tjckswhd777 <71741864+tjckswhd777@users.noreply.github.com>
2022-06-15 10:36:37 +0900

    neww

---
+++ PTSD_project/src/main/java/com/ptsd/controller/gpsService.java
@@ -0,0 +1,21 @@
+package com.ptsd.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/gpsService")
+public class gpsService extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String latitude = request.getParameter();
+		System.out.println(latitude);
+		
+	}
+
+}
--- PTSD_project/src/main/java/com/ptsd/controller/timeboardview.java
+++ PTSD_project/src/main/java/com/ptsd/controller/timeboardview.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/test")
-public class test extends HttpServlet {
+public class timeboardview extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response)
